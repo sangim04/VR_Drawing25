c815452c24fd416ecf77ac0cf2f444f000e893b3
azfrom1do <128368827+azfrom1do@users.noreply.github.com>
2025-04-22 03:31:19 +0900

    PlayerController, LayerManager scripts updata
    버튼 하나로 유저 상하 이동
    레이어 설정 스크립트
    Pen 스크립트는 테스트하느라 건든거니까 머지할때 Pen 스크립트는 무시해도 됨

--- Assets/Scripts/LayerManager.cs
+++ Assets/Scripts/LayerManager.cs
@@ -1,71 +1,93 @@
 using System.Collections.Generic;
 using NUnit.Framework;
 using Unity.VisualScripting;
+using System;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactors;
+using UnityEngine.XR.Interaction.Toolkit.Locomotion;
 
 public class LayerManager : MonoBehaviour
 {
-    public List<GameObject> layersZero;    // 0번 레이어
-    private List<GameObject> layersOne;     // 1번 레이어
-    private bool layersZeroFlag = true;     // 초기값은 보이게
-    private bool layersOneFlag = true;
-    // public bool test;
+    public InputActionReference primaryButton;      // A 버튼 
+    public InputActionReference secondaryButton;    // B 버튼 
+    
+    public int layerCount;                  // 레이어 갯수
+    private int setactiveLayerIndex = 0;    // 가시성 count
+    private int addLayerIndex = 0;          // 추가 count
+    // 중간에 layer 추기 기능 아직 없음
+    // 레이어 삭제 기능 아직 없음
+    
+    private static List<List<GameObject>> currentLayer = new List<List<GameObject>>();    // 현재 레이어
+    
+    /*
+     * setactiveLayerIndex는 몇번째 레이어가 보이게 할지 안보이게 할지
+     * addLayerIndex는 몇번 레이어에 선을 추가할지
+     */
 
     void Start()
     {
-        layersZero = new List<GameObject>();
-        layersOne = new List<GameObject>();
-    }
-
-    void FixedUpdate()
-    {
-        /*
-        if (test)
+        primaryButton.action.started += OnPrimaryStarted; // A 버튼 
+        //primaryButton.action.canceled += OnPrimaryReleased; // A 버튼 
+        secondaryButton.action.started += OnSecondaryStarted; // B 버튼 
+        //secondaryButton.action.canceled += OnSecondaryReleased; // B 버튼 
+        
+        for (int i = 0; i < layerCount; i++)    // layerCount만큼 레이어 생성
         {
-            test = false;
-            LayerSetActive(0);
-        }
-        */
-    }
-
-    public void AddLayer(GameObject layer, int layerNum)
-    {
-        switch (layerNum)
-        {
-            case 0:
-                layersZero.Add(layer);
-                break;
-            case 1:
-                layersOne.Add(layer);
-                break;
-            default:
-                Debug.Log("Layer Number Not Found");
-                break;
+            currentLayer.Add(new List<GameObject>());
         }
     }
 
-    public void LayerSetActive(int layerNum)
+    public void AddLayer(GameObject layer)  // 현재 레이어 List에 추가
     {
-        switch (layerNum)
+        currentLayer[addLayerIndex].Add(layer);
+    }
+
+    private void LayerSetActive()   // 레이어 가시성
+    {
+        setactiveLayerIndex++;
+        if(setactiveLayerIndex >= currentLayer.Count) setactiveLayerIndex = 0;
+        
+        if (setactiveLayerIndex == 0)   // 전부
         {
-            case 0:
-                foreach (var obj in layersZero)
+            foreach (var var in currentLayer)
+            {
+                foreach (var obj in var)
                 {
-                    obj.SetActive(!layersZeroFlag);
+                    obj.SetActive(!obj.activeSelf); // 보이면 안보이게, 안보이면 보이게
                 }
-                layersZeroFlag = !layersZeroFlag;
-                break;
-            case 1:
-                foreach (var obj in layersOne)
-                {
-                    obj.SetActive(!layersOneFlag);
-                }
-                layersOneFlag = !layersOneFlag;
-                break;
-            default:
-                Debug.Log("Layer Number Not Found");
-                break;
+            }
         }
+        else    // setactiveLayerIndex번째 레이어
+        {
+            foreach (var obj in currentLayer[setactiveLayerIndex])
+            {
+                obj.SetActive(!obj.activeSelf); // 보이면 안보이게, 안보이면 보이게
+            }
+        }
+    }
+
+    void OnPrimaryStarted(InputAction.CallbackContext context)
+    {
+        addLayerIndex++;
+        if (addLayerIndex >= layerCount) addLayerIndex = 0;
+    }
+
+    void OnPrimaryReleased(InputAction.CallbackContext context)
+    {
+        //Debug.Log("Primary Released");
+    }
+    
+    void OnSecondaryStarted(InputAction.CallbackContext context)
+    {
+        LayerSetActive();
+    }
+
+    void OnSecondaryReleased(InputAction.CallbackContext context)
+    {
+        //Debug.Log("Secondary Released");
     }
 }
 
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactors;
+using UnityEngine.XR.Interaction.Toolkit.Locomotion;
+
+public class PlayerController : MonoBehaviour
+{
+    public InputActionReference primaryButton;
+    public bool isPrimaryPressed; // X 버튼 상태 변수
+    public GameObject player;
+
+    private float upSpeed = 0.02f;
+    private bool isDownDelay;
+    private bool isDown;
+    
+    void Start()
+    {
+        primaryButton.action.started += OnPrimaryStarted; // X 버튼 
+        primaryButton.action.canceled += OnPrimaryReleased; // X 버튼 
+        isPrimaryPressed = false;
+        
+        player = GameObject.FindGameObjectWithTag("Player");    // XR Origin에 player태그 설정해야함
+    }
+
+    private void FixedUpdate()
+    {
+        if (isPrimaryPressed)
+        {
+            player.transform.position = new Vector3(player.transform.position.x, player.transform.position.y + (isDown ? -upSpeed:upSpeed), player.transform.position.z);
+            /*
+             * 위 아래 이동은 하는데 왼쪽 썸스틱으로 이동하면 떨어져요 : 밑을 보시오
+             * Locomotion  ->  Move  ->  Dynamic Move Provider 스크립트  ->  Awake 함수 안에 'useGravity = false;'를 넣어주세요
+             * Continuous Move Provider에 중력 계산이 있고 Dynamic Move Provider은 그걸 상속받습니다.
+             */
+        }
+    }
+    
+    void OnPrimaryStarted(InputAction.CallbackContext context)
+    {
+        isPrimaryPressed = true;
+        if (isDownDelay) isDown = true;         // isDownDelay가 true일 경우 내려감
+        else StartCoroutine(UpDown());    // isDownDelay가 false일 경우 n초동안 true가 되게 만드는 코루틴 호출
+    }
+
+    void OnPrimaryReleased(InputAction.CallbackContext context)
+    {
+        isPrimaryPressed = false;
+        isDown = false;
+    }
+
+    IEnumerator UpDown()    // 0.3f초동안 isDownDelay가 유지됨
+    {
+        isDownDelay = true;
+        yield return new WaitForSeconds(0.3f);
+        isDownDelay = false;
+    }
+}
